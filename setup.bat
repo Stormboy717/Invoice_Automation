ec48bede18cf52c4609e53da7ad1c7fdc13c8724
Sam <46669006+Stormboy717@users.noreply.github.com>
2023-06-13 09:42:19 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,6 @@
+msg * "Warning, please ensure you have the latest version of python 3 install from this websight https://www.python.org/downloads/ (this message will always show and is 
+not a check, the check is up to you)"
+curl http://raw.githubusercontent.com/Stormboy717/Invoice_Automation/main/Invoicely_Assistant.zip -LJO
+tar -xf ./Invoicely_Assistant.zip
+move ./"Invoicely Assistant" C:\ 
+pip install pillow faker
